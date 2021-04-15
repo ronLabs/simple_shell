f6a20b8915045c9a56b761bb6cd542b1a59e3ade
KennyReyesS <kennyreyes2000@gmail.com>
2021-04-15 18:40:15 +0000

    case PATH not found

--- searchpath.c
+++ searchpath.c
@@ -19,23 +19,23 @@ char *searchpath(char *cmd, char **env)
 	struct stat cmd_stat;
 
 	if (path == NULL)
-		path_notfound();
+	{
+		write(STDERR_FILENO, "PATH not found", 14);
+		exit(EXIT_FAILURE);
+	}
 	new_path = malloc(sizeof(char) * 100);
-	/* tokenizes and sends the tokens in path_array */
 	while (path_tokens != NULL)
 	{
 		path_array[i++] = path_tokens;
 		path_tokens = strtok(NULL, ":");
 	}
 	path_array[i] = NULL;
-	/* cmd entered by the user is inserted at the end of the directories*/
 	for (j = 0; path_array[j]; j++)
 	{
 		_strcpy(new_path, path_array[j]);
 		_strcat(new_path, "/");
 		_strcat(new_path, s2);
 		_strcat(new_path, "\0");
-		/* check if it exists */
 		if (stat(new_path, &cmd_stat) == 0)
 		{
 			free(path);
@@ -50,15 +50,4 @@ char *searchpath(char *cmd, char **env)
 	if (stat(cmd, &cmd_stat) == 0)
 	return (_strdup(cmd));
 	return (NULL);
-}
-
-/**
- *path_notfound - function in case there is no PATH.
- *
- */
-
-void path_notfound(void)
-{
-	write(STDERR_FILENO, "PATH not found", 14);
-	exit(EXIT_FAILURE);
 }
